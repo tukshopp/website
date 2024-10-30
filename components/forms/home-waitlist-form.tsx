32c75dfae0fee391e08fd5fca5f0cc4f769c5f60
Joshua <joshuanwafor01@gmail.com>
2024-10-30 12:28:38 +0100

    clean up

--- components/forms/home-waitlist-form.tsx
+++ components/forms/home-waitlist-form.tsx
@@ -46,7 +46,7 @@ const HomeWaitListForm = ({ activeStates }: HomeWaitListFormProps) => {
         });
       }
 
-      const res = await axios.post('http://staging-api.tukshopp.ng/v1/miscellaneous/waitlist', {
+      const res = await axios.post('https://staging-api.tukshopp.ng/v1/miscellaneous/waitlist', {
         contactName: '',
         contactEmail: userEmail,
         addressTag: userActiveState,
